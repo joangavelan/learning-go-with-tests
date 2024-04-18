6a76dd4fca01a5e7f78da1833178ec9afc118d83
joangavelan <joangavelan@gmail.com>
2024-04-17 19:38:09 -0500

    dependency injection

---
+++ dependency-injection/hello_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(os.Stdout, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
